de7c1cab97a89838408917db5d258d4712f4695b
Donell <donell.wambua@student.moringaschool.com>
2023-08-29 09:53:02 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears (name, age, sex)
+VALUES ('Mr. Chocolate', 5, 'M');
+
+INSERT INTO bears (name, age, sex)
+VALUES ('Rowdy', 3, 'M');
+
+-- Add similar statements for other bears
+INSERT INTO bears (name, age, sex)
+VALUES ('Mr. Chocolate', 5, 'M');
+
+INSERT INTO bears (name, age, sex)
+VALUES ('Rowdy', 3, 'M');
+
+-- Add similar statements for other bears
